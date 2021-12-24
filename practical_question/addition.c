8ec0af36fa4d4a11f1d35626081afeb71881810f
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2021-12-24 20:34:29 +0530

    Create addition.c

---
+++ practical_question/addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+int a ,b,sum;
+printf("Addition of 2 numbers :\n");
+printf("Enter first No.");
+scanf("%d",&a);
+printf("Enter second No.");
+scanf("%d",&b);
+sum = a +b;
+printf("Addition of %d and %d is : %d ",a,b,sum);
+return 0;
+}
