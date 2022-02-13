751b7cf6d4c3faeeb0fcf91cb3010954d36884d6
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-02-13 17:01:31 +0530

    Create stingfunction.c

---
+++ college_questions/stingfunction.c
@@ -0,0 +1,13 @@
+/*
+
+#include <string.h>
+    strcat()    not working
+    strrev()    not working 
+    strlen()    working 
+    strcpy()    not working 
+    strcmp()    working 
+    gets()
+    puts()
+  
+
+*/
