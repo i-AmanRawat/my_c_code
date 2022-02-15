80d623d5cf345351e4ca13594c14af226c075aaf
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-02-16 01:26:53 +0530

    Create 6.c

---
+++ practical_question/6.c
@@ -0,0 +1,10 @@
+// sum of first 20 natural numbers 
+#include<stdio.h>
+int main (){
+    int sum =0;
+    for (int i =1; i<=20;i++){
+        sum +=i;
+    }
+    printf("Sum of first 20 natural numbers is %d.",sum);
+    return 0;
+}
